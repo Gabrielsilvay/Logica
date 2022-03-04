3cfcb013d50ed4c2f6239ef8ce2632c9547749c1
Gabrielsilvay <matheusfdsantos95@gmail.com>
2022-03-03 21:13:33 -0300

    aula 03/03

---
+++ Prog.js
@@ -0,0 +1,8 @@
+
+//console.log('Prog.js');
+var email = 'dasilvagabriel22@gmail.com';
+var idade = 19; 
+var emailidade;
+
+emailidade = email + idade;
+console.log(emailidade);
---
+++ nome.js
@@ -0,0 +1,10 @@
+//Exibir meu nome
+//console.log('Gabriel');
+var nome = 'Gabriel ';
+var sobrenome = 'da Silva';
+//var nomecompleto;
+var idade = 48;
+/*nomecompleto = nome + sobrenome;
+console.log(nomecompleto);*/
+console.log( "Meu nome é:"+""+nome + " "+sobrenome );
+
