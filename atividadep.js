5046c00adebbb76a55ba978fc5da1de818777ab7
goob9009 <matheusfdsantos95@gmail.com>
2022-03-10 21:08:30 -0300

    atividade 10/03

---
+++ atividadep.js
@@ -0,0 +1,10 @@
+const alunos = 14;
+for (let i = 0; i <=alunos; i++) {
+if(i==0){
+        console.log("o numero é zero" + ' '+i)
+    }else if(i % 2==0){
+console.log("o numero é impar" + ' '+i)
+    }else {
+        console.log("o numero é par" + ' '+i)
+    }
+}
